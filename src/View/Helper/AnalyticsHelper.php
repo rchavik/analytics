595c440297c4e3f88b64defb50c79dece7700d8e
Walther Lalk <dakota@users.noreply.github.com>
2016-07-26 14:11:21 +0200

    Update to new analytics script

--- src/View/Helper/AnalyticsHelper.php
+++ src/View/Helper/AnalyticsHelper.php
@@ -19,17 +19,15 @@ class AnalyticsHelper extends Helper
         if (empty($webPropertyId)) {
             return;
         }
-        $domain = Configure::read('Site.Analytics.domain');
-        $identString = "_gaq.push(['_setAccount', '$webPropertyId']);";
-        if (!empty($domain)) {
-            $identString .= "_gaq.push(['_setDomainName', '$domain']);";
-        }
-        $script = sprintf("var _gaq = _gaq || []; %s _gaq.push(['_setAllowLinker', true]); _gaq.push(['_trackPageview']);
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();", $identString);
+        $identString = "ga('create', '{$webPropertyId}', 'auto');";
+        $script = "  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  {$identString}
+  ga('send', 'pageview');
+";
 
         $this->Html->scriptBlock($script, ['block' => true]);
     }
